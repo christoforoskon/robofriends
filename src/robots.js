42c067420267835d5cca129b12983e60382936d1
christoforoskon <xristoforos.knks@yahoo.gr>
2022-11-01 01:34:44 +0200

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,37 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Alex',
+    email: 'alex@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Christoforos',
+    email: 'christoforos@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'George',
+    email: 'george@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Maria',
+    email: 'maria@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Manos',
+    email: 'manos@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Didi',
+    email: 'didi@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Toni',
+    email: 'toni@gmail.com'
+  }
+]
